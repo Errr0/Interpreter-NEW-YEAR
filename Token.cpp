1e2cd653fa085af2c672a025cd1631b82d246db6
Errr0 <119783076+Errr0@users.noreply.github.com>
2025-09-05 21:15:41 +0200

    t

--- Token.cpp
+++ Token.cpp
@@ -1,18 +1,78 @@
+#include <iostream>
+
 enum class TokenType{
     Null,
     Function,
-    Variable
+    Int,
 };
 
-class Token{
-    public:
+enum class ReturnType {
+    Void,
+    Int
+};
+
+struct Token{
     TokenType type;
     void* value;
-    Token(TokenType type = TokenType::Null, void* value = nullptr, bool copyPtrValue = true){
+    Token(TokenType type = TokenType::Null, void* value = nullptr){
         this->type = type;
         this->value = value;
     }
-    ~Token(){
+    // ~Token(){
 
+    // }
+};
+
+struct FunctionObject {
+    using void_func = void(*)();
+    using int_func  = int(*)();
+    void* return_value;
+    ReturnType type;
+    void* func;
+
+    FunctionObject(void_func f) {
+        type = ReturnType::Void;
+        func = (void*)f;
+    }
+
+    FunctionObject(int_func f) {
+        type = ReturnType::Int;
+        func = (void*)f;
+    }
+
+    void call() {
+        switch (type) {
+            case ReturnType::Void:
+                ((void_func)func)();
+                break;
+
+            case ReturnType::Int: {
+                int result = ((int_func)func)();
+                return_value = (void*)&result;
+                std::cout << "Returned int: " << result << std::endl;
+                std::cout << "Returned int: " << *((int*)return_value) << std::endl;
+                break;
+            }
+        }
     }
 };
+
+// // --- przykładowe funkcje ---
+// void helloFunction() {
+//     std::cout << "Hello from void function!" << std::endl;
+// }
+
+// int numberFunction() {
+//     std::cout << "Hello from int function!" << std::endl;
+//     return 42;
+// }
+
+// int main() {
+//     FunctionObject f1(helloFunction);
+//     FunctionObject f2(numberFunction);
+
+//     f1.call();
+//     f2.call();
+
+//     return 0;
+// }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,99 @@
 #include <iostream>
 #include "token.cpp"
 
-int main(){
-    
+using void_func = void(*)();
+using int_func  = int(*)();
+
+struct FunctionObject {
+    using void_func = void(*)();
+    using int_func  = int(*)();
+    void* return_value;
+    ReturnType type;
+    void* func;
+
+    // Konstruktor dla void()
+    FunctionObject(void_func f) {
+        type = ReturnType::Void;
+        func = (void*)f;
+    }
+
+    // Konstruktor dla int()
+    FunctionObject(int_func f) {
+        type = ReturnType::Int;
+        func = (void*)f;
+    }
+
+    void call() {
+        switch (type) {
+            case ReturnType::Void:
+                ((void_func)func)();
+                break;
+
+            case ReturnType::Int: {
+                int result = ((int_func)func)();
+                return_value = (void*)&result;
+                std::cout << "Returned int: " << result << std::endl;
+                std::cout << "Returned int: " << *((int*)return_value) << std::endl;
+                break;
+            }
+        }
+    }
+};
+
+// --- przykładowe funkcje ---
+void helloFunction() {
+    std::cout << "Hello from void function!" << std::endl;
+}
+
+int numberFunction() {
+    std::cout << "Hello from int function!" << std::endl;
+    return 42;
+}
+
+int main() {
+    FunctionObject f1(helloFunction);
+    FunctionObject f2(numberFunction);
+
+    f1.call();
+    f2.call();
+
     return 0;
 }
+
+
+
+
+
+// struct FunctionObject {
+//     //type
+//     FunctionObject(){
+
+//     }
+//     void sayHello() {
+//         std::cout << "Hello from object!" << std::endl;
+//     }
+// };
+
+// int main() {
+//     FunctionObject obj;
+
+//     Token token = Token(TokenType::Function, (void*)&obj);
+
+//     // rzutowanie z powrotem
+//     ((FunctionObject*)token.value)->sayHello();
+
+//     return 0;
+// }
+
+// using void_func = void (*)();
+// using int_func = int (*)();
+
+// void helloFunction() {
+//     std::cout << "Hello from function!" << std::endl;
+// }
+
+// int main(){
+//     Token token = Token(TokenType::Function, (void*)&helloFunction);
+//     ((void_func)token.value)();
+//     return 0;
+// }
